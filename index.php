49416aba020032ad8df3e9b76d39fdc11ad24b74
mihail B <beltyukov-mihail@mail.ru>
2023-06-23 17:10:51 +0500

    asd

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             }else{
                 $file_name = 'img/ava/'.$_SESSION['mail'];
                 $image = $_SESSION['avatar'];
-                $a = '<div class="up_text font-size-2">'.'Welcome, '.$_SESSION['name'].'</div>';
+                $a = '<div class="test">'.'Welcome, '.$_SESSION['name'].'</div>';
             }
             include 'header.tpl';
         ?>
