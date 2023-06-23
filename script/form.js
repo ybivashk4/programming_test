2f8c47235b11a0f5b438ef3a771bae3a8fe5a184
mihail B <beltyukov-mihail@mail.ru>
2023-06-23 13:41:04 +0500

    Update form.js

--- script/form.js
+++ script/form.js
@@ -1,47 +1,7 @@
 let theme, flag = true;
 theme = document.getElementById("theme");
 // #5680E9 - #84CEEB - #5AB9EA - #C1C8E4 - #8860D0
-const changeTheme = function(){
-    if (flag){
-        document.body.style.background = "#84CEEB";
-        let upText = document.getElementsByClassName("up_text");
-        for (i=0;i<upText.length;i++){
-            upText[i].style.color = "#8860D0";
-        }
-        for (i=0;i<4;i++){
-        document.getElementsByClassName("down_text")[i].style.color = "#8860D0";
-        }
-        let img = document.getElementsByClassName("changeImage");
-        img[0].src = img[0].src.slice(0, img[0].src.indexOf("moonDark.png")) 
-        + "sunny.png";
 
-        let button = document.querySelectorAll('.but');
-        for(i=0;i<button.length;i++){
-            button[i].style.background = "#8860D0";
-            button[i].style.color = "#66FCF1";
-        }
-        flag = false;
-    }else{
-        document.body.style.background = "#202833";
-        let upText = document.getElementsByClassName("up_text");
-        for (i=0;i<upText.length;i++){
-            upText[i].style.color = "#66FCF1";
-        }
-        for (i=0;i<4;i++){
-        document.getElementsByClassName("down_text")[i].style.color = "#46A29F";
-        }
-        let img = document.getElementsByClassName("changeImage");
-        img[0].src = img[0].src.slice(0, img[0].src.indexOf("sunny.png")) 
-        + "moonDark.png";
-        let button = document.querySelectorAll('.but');
-        for(i=0;i<button.length;i++){
-            button[i].style.background = "#66FCF1";
-            button[i].style.color = "#202833";
-        }
-        flag = true;
-    }
-}
-theme.onclick = changeTheme;
 
 function createLabel(elem, name, textLabel, typeOfInput, pattertn){
     let test1 = document.createElement("label"),
